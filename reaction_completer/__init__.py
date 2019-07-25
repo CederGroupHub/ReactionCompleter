30c2f4d297b8f1e373183fd6cda9699a7e46626b
Haoyan Huo <hhaoyann@gmail.com>
2019-07-25 13:26:45 -0700

    add test for reaction completer

--- reaction_completer/__init__.py
+++ reaction_completer/__init__.py
@@ -1,3 +1,3 @@
-from reaction_completer.completer import ReactionCompleter
+from reaction_completer.completer import ReactionCompleter, balance_recipe
 from reaction_completer.errors import *
 from reaction_completer.material import MaterialInformation
